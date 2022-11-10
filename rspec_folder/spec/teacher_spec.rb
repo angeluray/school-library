6d587fef01f6c22aec36e59974777d3f78afe0e2
angeluray <aeucampos@gmail.com>
2022-11-10 11:07:02 -0400

    Added tests to main files

---
+++ rspec_folder/spec/teacher_spec.rb
@@ -0,0 +1,21 @@
+require_relative 'person_spec.rb'
+
+class Teacher < Person
+    attr_accessor :specialization
+  
+    def initialize(age, name, specialization)
+      super(age, name, parent_permission: true)
+      @specialization = specialization
+    end
+end
+
+describe Teacher do
+    context "When test the Teacher class" do
+        it "Creates a Teacher object" do
+            teacher = Teacher.new(28, "Christina Miller", "Science")
+            expect(teacher).to have_attributes(age: 28)
+            expect(teacher).to have_attributes(name: "Christina Miller")
+            expect(teacher).to have_attributes(specialization: "Science")
+        end
+    end
+end
